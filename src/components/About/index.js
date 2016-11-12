25130429c45a034efb89e0fedf3b29a0ff86a62f
Mathieu Gamache <mathg99@hotmail.com>
2016-11-12 14:53:58 -0500

    Setup route
    * Add react-router

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+
+
+ render() {
+    return (
+      <div>
+        <h1>
+          About
+        </h1>
+      </div>
+    );
+  }
+}
