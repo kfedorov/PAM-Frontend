6a91c39b37e7a31f8662f2b19fa27c24e6742f37
Mathieu <math.gamache@gmail.com>
2016-11-23 07:11:51 -0500

    Add support for auto complete
    * In fact it is auto-select (can't have value not in the options)
    * Doesn't feel ideal

--- src/components/App/SearchBar.jsx
+++ src/components/App/SearchBar.jsx
@@ -1,39 +1,74 @@
 import React, {Component} from 'react'
 
+// Import for react-select
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
 class SearchBar extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        
-        this.filtered = this.filtered.bind(this);
-        this.handleNameChange = this.handleNameChange.bind(this);
+
+        this.filtered = this
+            .filtered
+            .bind(this);
+        this.handleNameChange = this
+            .handleNameChange
+            .bind(this);
+        this.handleSelect = this
+            .handleSelect
+            .bind(this);
     }
 
-    filtered(name){
+    filtered(name) {
 
-        function contains(value){
-            return value.Name.toLowerCase().includes(name.toLowerCase());
+        function contains(value) {
+            return value
+                .Name
+                .toLowerCase()
+                .includes(name.toLowerCase());
         }
 
-        var filteredList = this.props.input.filter(contains);
+        var filteredList = this
+            .props
+            .input
+            .filter(contains);
 
-        this.props.callback(filteredList);
+        this
+            .props
+            .callback(filteredList);
     };
 
     handleNameChange(event) {
+
         this.filtered(event.target.value);
     };
+
+    handleSelect(selected) {
+        console.log(selected);
+        this.filtered(selected.value);
+    };
+
+    getSearchable(item) {
+        return {value: item.Name, label: item.Name};
+    }
 
     render() {
         return (
             <div>
                 <form>
                     <input type="text" onChange={this.handleNameChange}/>
+                    <Select
+                        type="text"
+                        onChange={this.handleSelect}
+                        options={this
+                        .props
+                        .input
+                        .map(this.getSearchable)}/>
                 </form>
             </div>
         );
     }
 }
-
 
 export default SearchBar
