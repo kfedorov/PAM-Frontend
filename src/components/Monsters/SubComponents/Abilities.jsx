56490efb8378a228f5bcdf30efa9d6db1cc61e35
Mathieu <math.gamache@gmail.com>
2016-12-11 17:15:09 -0500

    Beautify abilities

--- src/components/Monsters/SubComponents/Abilities.jsx
+++ src/components/Monsters/SubComponents/Abilities.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 
 
+function modifierCalculator(value) {
+    var modifier;
+    if(value > 10)
+    {
+        value = value - 10;
+        modifier = "+" + Math.floor(value/2); 
+    }
+    else{
+        value = value - 10;
+        modifier = Math.floor(value/2); 
+    }
+
+    return modifier;
+}
+
 const Ability = ({name, value}) => {
     return (
-        <div>
-            <span>{name}:{value}</span>
+        <div className="ability">
+          <div>
+            <b>{ name.substring(0, 3).toUpperCase() }</b>
+          </div>
+          <div>
+            <span>{ value } ({ modifierCalculator(value) })</span>
+          </div>
         </div>
-    );
+        );
 
 }
 
@@ -14,16 +34,18 @@ const Ability = ({name, value}) => {
 const Abilities = ({abilities}) => {
     return (
         <div>
-            <h2>Abilities</h2>
-            <hr/>
-            <Ability name="Strength" value={abilities.strength} />
-            <Ability name="Dexterity" value={abilities.dexterity} />
-            <Ability name="Constitution" value={abilities.constitution} />
-            <Ability name="Intelligence" value={abilities.intelligence} />
-            <Ability name="Wisdom" value={abilities.wisdom} />
-            <Ability name="Charisma" value={abilities.charisma} />
+          <h2>Abilities</h2>
+          <hr/>
+          <div className="abilities">
+            <Ability name="Strength" value={ abilities.strength } />
+            <Ability name="Dexterity" value={ abilities.dexterity } />
+            <Ability name="Constitution" value={ abilities.constitution } />
+            <Ability name="Intelligence" value={ abilities.intelligence } />
+            <Ability name="Wisdom" value={ abilities.wisdom } />
+            <Ability name="Charisma" value={ abilities.charisma } />
+          </div>
         </div>
-    );
+        );
 
 }
 
