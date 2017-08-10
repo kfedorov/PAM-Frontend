1f1e1d945575c7deb533c92a26701aba9a3506f6
Mathieu Gamache <mathg99@hotmail.com>
2017-08-09 21:02:17 -0400

    Fix router upgrade issue

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 /* Utils */
 import React from "react";
 import ReactDOM from "react-dom";
-import { browserHistory } from "react-router";
 
 /* Components */
 import Routes from "./routes";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
+import "react-tabs/style/react-tabs.css";
 
 /* Style */
 import "./index.css";
 import "react-select/dist/react-select.css";
 
-ReactDOM.render(
-  <Routes history={browserHistory} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Routes />, document.getElementById("root"));
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
+/* Temporary not-used: will need some refactoring */
+
 /* Utils */
-import React from 'react';
-import {Router, Route} from 'react-router';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 /* Components (possible routes) */
-import App from './containers/App';
-import About from './containers/About';
-import NotFound from './containers/NotFound';
+import App from "./containers/App";
+import About from "./containers/About";
+import NotFound from "./containers/NotFound";
 
-const Routes = (props) => (
-    <Router {...props}>
-        <Route path='/' component={App} />
-        <Route path='/about' component={About} />
-        <Route path='*' component={NotFound} />
-    </Router>
-
-
-);
-
+const Routes = props =>
+  <Router {...props}>
+    <main>
+      <Route exact path="/" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="*" component={NotFound} />
+    </main>
+  </Router>;
 
 export default Routes;
