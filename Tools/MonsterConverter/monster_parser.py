2bd9e0e37b96d2436bf64f7de77b1b0d2d6fcebd
Mathieu Gamache <mathg99@hotmail.com>
2018-01-23 16:04:19 -0500

    Complete monster crawler/parser

--- Tools/MonsterConverter/monster_parser.py
+++ Tools/MonsterConverter/monster_parser.py
@@ -16,24 +16,48 @@ ABILITIES_RULE = r'\|\s*(\d*)\s\('
 SAVING_THROWS_RULE = r'\*\*Saving Throws\*\*(.*)'
 SKILLS_RULE = r'\*\*Skills\*\*(.*)'
 
+DAMAGE_VULNERABILITIES_RULE = r"\*\*Damage Vulnerabilities\*\*(.*)"
+DAMAGE_RESISTANCE_RULE = r"\*\*Damage Resistances\*\*(.*)"
+DAMAGE_IMMUNITIES_RULE = r"\*\*Damage Immunities\*\*(.*)"
+CONDITION_IMMUNITIES_RULE = r"\*\*Condition Immunities\*\*(.*)"
+
+SENSES_RULE = r"\*\*Senses\*\*(.*)"
+LANGUAGES_RULE = r"\*\*Languages\*\*(.*)"
+
+ACTIONS_AND_LEGENDARY_ACTIONS_SECTIONS_RULE = r"\*\*Actions\*\*.*\n\n(?:.*\n*)*"
+LEGENDARY_ACTIONS_SECTIONS_RULE = r"\*\*Legendary Actions\*\*.*\n\n(?:.*\n*)*"
+NAME_DESCRIPTION_EXTRACTOR_RULE = r"\*\*\*(.*)\*\*\*(.*\n*.*)"
+
 current_path = ""
 
-def get_value(text, rule, match_group = 1, isOptional = False,):
+def get_value(text, rule, match_group=1, is_optional=False,):
     match = re.search(rule, text, re.M | re.I | re.M)
     if match:
         return match.group(match_group)
-    elif not isOptional:
+    elif not is_optional:
         print("Can't parse " + current_path + " with rule " + rule)
+    return None
 
-def get_all_values(text, rule, match_group = 1, isOptional = False):
+def get_all_values(text, rule, is_optional=False):
     matches = re.findall(rule, text, re.M | re.I | re.M)
     results = []
     if matches:
         for match in matches:
             results.append(match)
         return results
-    elif not isOptional:
+    elif not is_optional:
         print("Can't parse " + current_path + " with rule " + rule)
+    return []
+
+def get_enumeration(text, rule, match_group=1, is_optional=False,):
+    match = re.search(rule, text, re.M | re.I | re.M)
+    if match:
+        match_to_split = match.group(match_group)
+        values = match_to_split.strip().replace(";", ",").split(",")
+        return [s.strip() for s in values]
+    elif not is_optional:
+        print("Can't parse " + current_path + " with rule " + rule)
+    return []
 
 def contain(text, rule):
     match = re.search(rule, text, re.M | re.I | re.M)
@@ -42,6 +66,26 @@ def contain(text, rule):
     else:
         return False
 
+def non_magicial_weapon_tricks(array):
+
+    non_magical_str = next((x for x in array if "magical" in x), None)
+
+    if non_magical_str:
+        array = [s for s in array if "slashing" not in s and "bludgeoning" not in s and "piercing" not in s]
+        array.append("bludgeoning, piercing " + non_magical_str)                
+    return array
+
+def name_description_extractor_tricks(text):
+    splitted = re.split("\*\*\*(.*)\*\*\*", text)
+    index = 1
+    pairs = [] 
+    while index < len(splitted):
+        pair = {}
+        pair["name"] = splitted[index].strip()
+        pair["desc"] = splitted[index + 1].strip()
+        pairs.append(pair)
+        index = index + 2
+    return pairs
 
 def parse_monster(path):
     global current_path
@@ -75,7 +119,7 @@ def parse_monster(path):
     # HP
     monster["hitPoints"] = (int)(get_value(all_text, HP_RULE))
 
-    monster["hitDice"] = get_value(all_text, HIT_DICE_RULE, isOptional = True)
+    monster["hitDice"] = get_value(all_text, HIT_DICE_RULE, is_optional = True)
 
     # AC
     monster["speed"] = (int)(get_value(all_text, ARMOR_CLASS_RULE))
@@ -94,7 +138,7 @@ def parse_monster(path):
     monster["abilities"] = abilities
 
     # Saving throws
-    saving_throws_result = get_value(all_text, SAVING_THROWS_RULE, isOptional = True)
+    saving_throws_result = get_value(all_text, SAVING_THROWS_RULE, is_optional = True)
 
     saving_throws = []
 
@@ -114,7 +158,7 @@ def parse_monster(path):
 
 
     # Skills
-    skills_result = get_value(all_text, SKILLS_RULE, isOptional = True)
+    skills_result = get_value(all_text, SKILLS_RULE, is_optional = True)
 
     skills = []
 
@@ -134,20 +178,54 @@ def parse_monster(path):
 
     # Resistances
 
+    resistances = {}
+    resistances["damageVulnerabilities"] = non_magicial_weapon_tricks(get_enumeration(all_text, DAMAGE_VULNERABILITIES_RULE, is_optional = True))
+    resistances["damageResistance"] = non_magicial_weapon_tricks(get_enumeration(all_text, DAMAGE_RESISTANCE_RULE, is_optional = True))
+    resistances["damageImmunities"] = non_magicial_weapon_tricks(get_enumeration(all_text, DAMAGE_IMMUNITIES_RULE, is_optional = True))
+    resistances["conditionImmunities"] = get_enumeration(all_text, CONDITION_IMMUNITIES_RULE, is_optional = True)
+    monster["resistances"] = resistances
+
+    # Sections manipulation
+    legendary_section = get_value(all_text, LEGENDARY_ACTIONS_SECTIONS_RULE, match_group=0, is_optional=True)
+
+    if legendary_section is None:
+        legendary_section = ""
+    
+    action_and_legendary_section = get_value(all_text, ACTIONS_AND_LEGENDARY_ACTIONS_SECTIONS_RULE, match_group=0, is_optional=True)
+    
+    if action_and_legendary_section is None:
+        action_and_legendary_section = ""
+
+    action_section = action_and_legendary_section.replace(legendary_section, "")
+
+    feature_section = all_text.replace(action_and_legendary_section, "")
+
     # Features
+    monster["features"] = name_description_extractor_tricks(feature_section)
 
     # Actions
+    monster["actions"] = name_description_extractor_tricks(action_section)
 
     # Legendary Actions
+    monster["legendaryActions"] = name_description_extractor_tricks(legendary_section)
 
     # Sense
+    senses = get_value(all_text, SENSES_RULE, is_optional=True)
+    if senses:
+        monster["senses"] = senses.strip()
+    else:
+        monster["senses"] = ""
 
     # Language
-
+    languages = get_value(all_text, LANGUAGES_RULE, is_optional=True)
+    if languages:
+        monster["languages"] = languages.strip()
+    else:
+        monster["languages"] = ""
 
     return monster
 
 
 monster_path=os.path.join(
-    os.getcwd(), r"bestiary_temp\_posts\2017-09-10-aboleth.markdown")
+    os.getcwd(), r"bestiary_temp\_posts\2017-09-10-lich.markdown")
 parse_monster(monster_path)
